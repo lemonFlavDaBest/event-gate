3fdc49afe294187df3305e5fc073a727db37b5d3
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-03-23 15:40:45 -0700

    Update EventGate.sol
    adding new events, adding a new variable about whether an event has started,

--- packages/hardhat/contracts/EventGate.sol
+++ packages/hardhat/contracts/EventGate.sol
@@ -33,15 +33,20 @@ contract EventGate is Ownable {
     mapping(uint256 => EventInfo) public events;
     mapping(bytes32 => uint256) public eventHashToEventId;
     mapping(bytes32 => bool) public eventExists;
+    mapping(uint256 => bool) public eventInProgress;
+    mapping(uint256 => address) public eventsOwner;
 
 
-    event EventCreated(uint256 indexed eventId, bytes32 indexed eventHash, address ticketAddress, string eventName);
+
+    event EventCreated(uint256 indexed eventId, bytes32 indexed eventHash, address ticketAddress, string eventName, address eventOwner);
     event EventEntered(uint256 indexed eventId, bytes32 eventHash, bytes32 indexed entrantHash, 
     uint256 indexed eventTicketId, address entrant, address ticketAddress, string eventName, uint256 time);
+    event EventStarted(uint256 indexed eventId, uint256 time)
+    event EventFinished(uint256 indexed eventId, uint256 time)
 
     constructor(){}
 
-    function createEvent(address _ticketAddress, string calldata _eventName) external returns(uint256){
+    function createEvent(address _ticketAddress, string calldata _eventName, bool startEvent) external returns(uint256){
       bytes32 _eventHash = keccak256(abi.encode(_ticketAddress,_eventName));
       require(eventExists[_eventHash] != true, 'event already exists');
       eventIdCounter.increment();
@@ -54,19 +59,32 @@ contract EventGate is Ownable {
       _eventInfo.eventId = _eventId;
       _eventInfo.eventCreator = msg.sender;
       _eventInfo.eventHash = _eventHash;
-      emit EventCreated(_eventId, _eventHash, _ticketAddress, _eventName);
+      eventInProgress[_eventId] = startEvent;
+      eventsOwner[_eventId] = msg.sender; 
+      emit EventCreated(_eventId, _eventHash, _ticketAddress, _eventName, msg.sender);
+      if (startEvent) emit EventStarted(_eventId, block.timestamp);
       return _eventId;
     }
 
-    function enterEvent(address _ticketAddress, string calldata _eventName, uint256 _eventTicketId) external returns(bytes32){
+    function enterEvent(address _ticketAddress, string calldata _eventName, uint256 _eventTicketId, uint256 _eventId) external returns(bytes32){
       require(IERC721(_ticketAddress).ownerOf(_eventTicketId) == msg.sender, "You must own the token you are entering with");
       bytes32 _eventHash = keccak256(abi.encode(_ticketAddress,_eventName));
       require(eventExists[_eventHash], "This event does not exist");
+      require(eventInProgress[_eventId], "event has not started");
       bytes32 _entrantHash = keccak256(abi.encode(msg.sender,_eventTicketId));
-      uint256 _eventId = eventHashToEventId[_eventHash];
       emit EventEntered(_eventId, _eventHash, _entrantHash, _eventTicketId, msg.sender, _ticketAddress, _eventName, block.timestamp);
       return _entrantHash;
     }
 
+    function changeEventStatus(uint256 _eventId) external {
+      require(eventsOwner[_eventId] == msg.sender, "only event owner can call");
+      if (eventInProgress[_eventId]) {
+        eventInProgress[_eventId] = false;
+        emit EventFinished(_eventId, block.timestamp);
+      } else {
+        eventInProgress[_eventId] = true;
+        emit EventStarted(_eventId, block.timestamp)
+      }
+    }
     receive() external payable {}
 }
