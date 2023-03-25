c4ec2f719269a8193ad492905991f8a7a6717b0f
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-03-25 16:44:25 -0700

    Create ETHGlobalTicket.sol
    change name of file

---
+++ packages/hardhat/contracts/ETHGlobalTicket.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+/**
+ * @title DamnValuableNFT
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ * @notice Implementation of a mintable and burnable NFT with role-based access controls
+ */
+contract ETHGlobalTicket is ERC721, ERC721Burnable {
+    uint256 public tokenIdCounter;
+
+    constructor() ERC721("ETHGlobalTicket", "ETIX") {
+    }
+
+    function safeMint(address to) public returns (uint256 tokenId) {
+        tokenId = tokenIdCounter;
+        _safeMint(to, tokenId);
+        ++tokenIdCounter;
+    }
+}
