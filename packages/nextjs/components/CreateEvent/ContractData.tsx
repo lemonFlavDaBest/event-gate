46ebbe0fca90282a61be38fad7a8cdf0807ca98c
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-03-25 08:11:56 -0700

    Update ContractData.tsx
    trying to get rid of unnecessary variables and requires from the template

--- packages/nextjs/components/CreateEvent/ContractData.tsx
+++ packages/nextjs/components/CreateEvent/ContractData.tsx
@@ -1,40 +1,42 @@
-import { useEffect, useRef, useState } from "react";
-import Marquee from "react-fast-marquee";
+import { SetStateAction, useEffect, useRef, useState } from "react";
 import { useScaffoldContractRead, useScaffoldEventSubscriber } from "~~/hooks/scaffold-eth";
 import { BigNumber } from "ethers";
 import { useAnimationConfig } from "~~/hooks/scaffold-eth/useAnimationConfig";
+import PartyIcon from "./assets/PartyPopper.svg";
+import { Address, Unit } from "wagmi";
 
 const MARQUEE_PERIOD_IN_SEC = 5;
 
 export default function ContractData() {
   const [transitionEnabled, setTransitionEnabled] = useState(true);
-  const [isRightDirection, setIsRightDirection] = useState(false);
+  //const [isRightDirection, setIsRightDirection] = useState(false);
   const [marqueeSpeed, setMarqueeSpeed] = useState(0);
-  const [eventReciept, setEventReceipt] = useState({})
+  const [eventFound, setEventFound] = useState<boolean>(false);
+  const [eventReciept, setEventReciept] = useState({})
+  const [deventId, setDeventId] = useState<unknown>()
 
   const containerRef = useRef<HTMLDivElement>(null);
   const greetingRef = useRef<HTMLDivElement>(null);
 
   const { data: totalCounter } = useScaffoldContractRead<BigNumber>("YourContract", "totalCounter");
-
-  const { data: currentGreeting, isLoading: isGreetingLoading } = useScaffoldContractRead<string>(
-    "YourContract",
-    "greeting",
-  );
+  const { data: eventIdCounter } = useScaffoldContractRead<BigNumber>("EventGate", "EventIdCounter");
+ 
 
   useScaffoldEventSubscriber("YourContract", "GreetingChange", (greetingSetter, newGreeting, premium, value) => {
     console.log(greetingSetter, newGreeting, premium, value);
   });
   useScaffoldEventSubscriber("EventGate", "EventCreated", (eventId, eventHash, ticketAddress, eventName, eventOwner) => {
     console.log(eventId, eventHash, ticketAddress, eventName, eventOwner);
-    setEventReceipt({...eventReciept, "eventId": eventId, "eventHash":eventHash, "ticketAddress": ticketAddress, 
-                      "eventName": eventName, "eventOwner": eventOwner})
+    setDeventId(eventId)
+    setEventReciept({"eventId" : eventId , "eventHash": eventHash, "ticketAddress": ticketAddress, "eventName":eventName,
+      "eventOwner": eventOwner});
+      setEventFound(true);
   });
 
 
   const { showAnimation } = useAnimationConfig(totalCounter);
 
-  const showTransition = transitionEnabled && !!currentGreeting && !isGreetingLoading;
+  //const showTransition = transitionEnabled && !!currentGreeting && !isGreetingLoading;
 
   useEffect(() => {
     if (transitionEnabled && containerRef.current && greetingRef.current) {
@@ -45,45 +47,37 @@ export default function ContractData() {
   }, [transitionEnabled, containerRef, greetingRef]);
 
   return (
-    <div className="flex flex-col justify-center items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-10 px-5 sm:px-0 lg:py-auto max-w-[100vw] ">
-      <div
+    
+    <div className="flex flex-col items-center bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] py-5 px-5 sm:px-0 lg:py-auto max-w-[100vw]">
+
+      {eventFound &&<div
         className={`flex flex-col max-w-md bg-base-200 bg-opacity-70 rounded-2xl shadow-lg px-5 py-4 w-full ${
           showAnimation ? "animate-zoom" : ""
         }`}
       >
+        
         <div className="flex justify-end w-full">
           <div className="bg-secondary border border-primary rounded-xl flex">
-            <div className="p-2 py-1 border-r border-primary flex items-end">Total count</div>
+            <div className="p-2 py-1 border-r border-primary flex items-end">Event ID</div>
             <div className="text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end font-bai-jamjuree">
-              {totalCounter?.toString() || "0"}
+              {eventFound ? eventIdCounter.toNumber() + 1 : "?"}
             </div>
           </div>
         </div>
 
-        <div className="mt-3 border border-primary bg-neutral rounded-3xl text-secondary  overflow-hidden text-[116px] whitespace-nowrap w-full uppercase tracking-tighter font-bai-jamjuree leading-tight">
-          <div className="relative overflow-x-hidden" ref={containerRef}>
-            {/* for speed calculating purposes */}
-            <div className="absolute -left-[9999rem]" ref={greetingRef}>
-              <div className="px-4">{currentGreeting}</div>
+        <div className="card mt-3 border border-primary bg-neutral rounded-3xl w-90 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img src='https://openmoji.org/data/color/svg/1F389.svg' alt="Party Popper" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Event Made!</h2>
+            <p></p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Start Event</button>
             </div>
-            {new Array(3).fill("").map((_, i) => {
-              const isLineRightDirection = i % 2 ? isRightDirection : !isRightDirection;
-              return (
-                <Marquee
-                  key={i}
-                  direction={isLineRightDirection ? "right" : "left"}
-                  gradient={false}
-                  play={showTransition}
-                  speed={marqueeSpeed}
-                  className={i % 2 ? "-my-10" : ""}
-                >
-                  <div className="px-4">{currentGreeting || " "}</div>
-                </Marquee>
-              );
-            })}
           </div>
         </div>
-      </div>
-    </div>
+      </div>}
+    </div> 
   );
 }
