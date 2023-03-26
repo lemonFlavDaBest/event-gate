6f00ce6c1009e4c9b0e113f6b37d0608917c565a
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-03-25 23:24:03 -0700

    creating routes

---
+++ packages/nextjs/pages/guard/[event].tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+
+import Head from "next/head";
+import { ContractData } from "~~/components/GuardEvent";
+
+const Guard: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>dEvent Guard and Ticket Check</title>
+        <meta name="description" content="Checking for tickets" />
+        {/* We are importing the font this way to lighten the size of SE2. */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="grid flex-grow" data-theme="exampleUi">
+        <ContractData />
+      </div>
+    </>
+  );
+};
+
+export default Guard;
---
+++ packages/nextjs/pages/view-events.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { ContractData } from "~~/components/ViewEvents";
+
+const ViewEvents: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>View My Events</title>
+        <meta name="description" content="View your events you have created" />
+        {/* We are importing the font this way to lighten the size of SE2. */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="grid flex-grow" data-theme="exampleUi">
+        <ContractData />
+      </div>
+    </>
+  );
+};
+
+export default ViewEvents;
