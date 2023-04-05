31569c3309b2c07948ffed61ba36272c7670c814
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-04-05 07:51:26 -0700

    Update index.tsx
    setting up a grid for my hero section

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -12,14 +12,22 @@ const Home: NextPage = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet" />
       </Head>
-      <img src='image.png' className="absolute top-24"></img>
+      
       <div className="flex bg-[url('/assets/gradient-2.png')] bg-[length:100%_100%] flex-col flex-grow pt-10 lg:py-auto max-w-[100vw]">
-        
+      <div className="grid lg:grid-cols-2 items-center justify-items-center gap-5">
         <div className="px-5 py-20">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-8xl font-bold">dEvents</span>
           </h1>
+        </div>
+        <div className="px-5 py-20">
+          <h3 className="text-center mb-8">
+            <span className="block text-4xl font-bold mb-2">Event Ticketing</span>
+            <span className="block text-4xl ">And Admissions</span>
+            <span className="block text-2xl ">Made Simple.</span>
+          </h3>   
+        </div>
         </div>
         <div className="px-5 py-20">
           <h3 className="text-center mb-8">
@@ -34,6 +42,7 @@ const Home: NextPage = () => {
             <span className="block text-8xl font-bold">dEvents</span>
           </h2>   
         </div>
+    
     </div>
     </>
   );
