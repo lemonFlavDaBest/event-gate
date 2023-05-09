bbaebf5c4446d9699e44332dfc56780ceabed47c
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-05-09 14:07:56 -0700

    Update search-event.tsx
    changin font and title of pages and their desrcription from the template to the specific page

--- packages/nextjs/pages/search-event.tsx
+++ packages/nextjs/pages/search-event.tsx
@@ -6,11 +6,11 @@ const SearchEvent: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Scaffold-eth Example Ui</title>
-        <meta name="description" content="Created with 🏗 scaffold-eth" />
+        <title>Search For DEvent</title>
+        <meta name="description" content="Find your DEvent" />
         {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet" />
       </Head>
       <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
         <ContractInteraction />
