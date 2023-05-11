8aa8143c58612cf386529df20a13fadd9aa80c37
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-05-11 15:11:32 -0700

    Update [event].tsx
    Change Title for styling change and also change the font for the page

--- packages/nextjs/pages/events/[event].tsx
+++ packages/nextjs/pages/events/[event].tsx
@@ -7,11 +7,11 @@ const EventId: NextPage = () => {
   return (
     <>
       <Head>
-        <title>dEvent Info</title>
-        <meta name="description" content="Get dEvent info and description" />
+        <title>DEvent Info</title>
+        <meta name="description" content="Get DEvent info and description" />
         {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet" />
       </Head>
       <div className="grid flex-grow" data-theme="exampleUi">
         <ContractData />
