58473464bfd78c9e41bf08d80f643b923d786cdb
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-05-11 15:09:40 -0700

    Update [event].tsx
    change title of this page as well as the font

--- packages/nextjs/pages/guard/[event].tsx
+++ packages/nextjs/pages/guard/[event].tsx
@@ -7,11 +7,11 @@ const Guard: NextPage = () => {
   return (
     <>
       <Head>
-        <title>dEvent Guard and Ticket Check</title>
+        <title>DEvent Ticket Check</title>
         <meta name="description" content="Checking for tickets" />
         {/* We are importing the font this way to lighten the size of SE2. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet" />
       </Head>
       <div className="grid flex-grow" data-theme="exampleUi">
         <ContractData />
